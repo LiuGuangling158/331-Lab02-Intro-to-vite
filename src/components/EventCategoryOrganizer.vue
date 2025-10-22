d81055f6159ff86588453ff28052a7d63af13566
wurunxin <3282982622@qq.com>
2025-10-22 20:27:49 +0800

    db

---
+++ src/components/EventCategoryOrganizer.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { Event } from '@/types'
+
+defineProps<{
+  event: Event
+}>()
+</script>
+
+<template>
+  <div class="event-category-organizer">
+    <div class="info-container">
+      <span class="category">{{ event.category }}</span>
+      <span class="organizer">{{ event.organizer }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.event-category-organizer {
+  width: 250px;
+  margin-bottom: 18px;
+}
+
+.info-container {
+  text-align: right;
+}
+
+.category,
+.organizer {
+  font-size: 14px; /* 相同的字体大小 */
+  display: block; /* 使每个元素占一行 */
+  margin-bottom: 4px;
+}
+
+.organizer {
+  margin-bottom: 0;
+}
+</style>
