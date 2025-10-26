9a24908be640f4899bf1c9b2befbf909edd6077b
wurunxin <3282982622@qq.com>
2025-10-26 11:14:24 +0800

    lab3

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,16 +1,20 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: '[your mock server url]',
+  baseURL: 'https://my-json-server.typicode.com/LiuGuangling158/331-Lab02-Intro-to-vite',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
-  }
+  },
+  timeout: 5000 // 5秒超时
 })
 
 export default {
-  getEvents() {
-    return apiClient.get('/events')
+  getEvents(perPage: Number, page: Number) {
+    return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
+  },
+  getEvent(id: number) {
+    return apiClient.get('/events/' + id)
   }
 }
